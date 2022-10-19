16f76fbd8b8bbe0928063b71d9b314709b05c0ba
오준솔 <>
2022-10-19 21:06:22 +0900

    hello world

---
+++ Pokemon-Dictionary/Pokemon-Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon-Dictionary
+//
+//  Created by 오준솔 on 2022/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
